875271b942b6503ec7185b416a960c9014987a53
Martijn Faassen <faassen@startifact.com>
2023-03-29 17:55:21 +0200

    Solve a bug where names weren't parsed correctly

--- xee-xpath/src/parse.rs
+++ xee-xpath/src/parse.rs
@@ -23,10 +23,26 @@ mod tests {
     }
 
     #[test]
-    fn test_xpath() {
-        let successful_parse = XPathParser::parse(Rule::Xpath, "/a/c");
-        assert!(successful_parse.is_ok());
+    fn test_not_xpath() {
         let unsuccessful_parse = XPathParser::parse(Rule::Xpath, ")(");
         assert!(unsuccessful_parse.is_err());
+    }
+
+    #[test]
+    fn test_xpath_simple_name() {
+        let successful_parse = XPathParser::parse(Rule::Xpath, "foo");
+        assert!(successful_parse.is_ok());
+    }
+
+    #[test]
+    fn test_xpath_relative_path() {
+        let successful_parse = XPathParser::parse(Rule::Xpath, "foo/bar");
+        assert!(successful_parse.is_ok());
+    }
+
+    #[test]
+    fn test_xpath_absolute_path() {
+        let successful_parse = XPathParser::parse(Rule::Xpath, "/foo/bar");
+        assert!(successful_parse.is_ok());
     }
 }
