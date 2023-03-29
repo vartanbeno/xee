1a73c021b4a70aa7d5aa55f833f3d0fa6d397cb6
Martijn Faassen <faassen@startifact.com>
2023-03-29 16:02:52 +0200

    starting to implement a pest parser

---
+++ xee-xpath/src/parse.rs
@@ -0,0 +1,17 @@
+#[derive(Parser)]
+#[grammar = "xpath-31.pest"]
+pub struct XPathParser;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pest::Parser;
+
+    #[test]
+    fn test_char() {
+        let successful_parse = XPathParser::parse(Rule::Char, "abc");
+        assert!(successful_parse.is_ok());
+        let unsuccessful_parse = XPathParser::parse(Rule::Char, "\u{10}");
+        assert!(unsuccessful_parse.is_err());
+    }
+}
