c3ac8d4f2e8c2a58c5eb88884cb449ab216bf335
Martijn Faassen <faassen@startifact.com>
2023-04-25 16:52:12 +0200

    It's starting to do something

--- xee-xpath/src/ir_interpret.rs
+++ xee-xpath/src/ir_interpret.rs
@@ -1,4 +1,5 @@
 use crate::ast;
+use crate::ast_ir::Converter;
 use crate::builder::{BackwardJumpRef, Comparison, FunctionBuilder, JumpCondition, Program};
 use crate::context::Context;
 use crate::error::Result;
@@ -6,8 +7,6 @@ use crate::instruction::Instruction;
 use crate::interpret::Interpreter;
 use crate::ir;
 use crate::parse_ast::parse_xpath;
-// use crate::scope::Scopes;
-use crate::ast_ir::Converter;
 use crate::value::{Atomic, FunctionId, Item, Node, StackValue};
 
 type Scopes = crate::scope::Scopes<ir::Name>;
@@ -86,6 +85,8 @@ impl<'a> InterpreterCompiler<'a> {
     }
 
     fn compile_binary(&mut self, binary: &ir::Binary) {
+        self.compile_atom(&binary.left);
+        self.compile_atom(&binary.right);
         match &binary.op {
             ir::BinaryOp::Add => {
                 self.builder.emit(Instruction::Add);
@@ -158,6 +159,7 @@ impl<'a> InterpreterCompiler<'a> {
     }
 
     fn compile_function_call(&mut self, function_call: &ir::FunctionCall) {
+        self.builder.emit(Instruction::PrintStack);
         self.compile_atom(&function_call.atom);
         for arg in &function_call.args {
             self.compile_atom(arg);
@@ -178,6 +180,8 @@ impl<'a> CompiledXPath<'a> {
         let ast = parse_xpath(xpath);
         let mut converter = Converter::new(&context.static_context);
         let expr = converter.convert_xpath(&ast);
+        // we get an inline function, unwrap it for now
+        let (arg_name, expr) = unwrap_inline_function(expr);
 
         let mut program = Program::new();
         let mut scopes = Scopes::new(ir::Name("dummy".to_string()));
@@ -187,9 +191,9 @@ impl<'a> CompiledXPath<'a> {
             scopes: &mut scopes,
             context,
         };
+        compiler.scopes.push_name(&arg_name);
         compiler.compile_expr(&expr);
-        // call the inline function this defines
-        compiler.builder.emit(Instruction::Call(1));
+
         let main = compiler.builder.finish("main".to_string(), 0);
         let main = program.add_function(main);
         Self {
@@ -223,6 +227,16 @@ impl<'a> CompiledXPath<'a> {
 
     pub(crate) fn interpret_with_xot_node(&self, node: xot::Node) -> Result<StackValue> {
         self.interpret_with_context(Item::Node(Node::Xot(node)))
+    }
+}
+
+fn unwrap_inline_function(expr: ir::Expr) -> (ir::Name, ir::Expr) {
+    match expr {
+        ir::Expr::FunctionDefinition(ir::FunctionDefinition { params, body, .. }) => {
+            assert_eq!(params.len(), 1);
+            (params[0].0.clone(), *body)
+        }
+        _ => panic!("expected inline function"),
     }
 }
 
@@ -260,14 +274,13 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn test_compile_add() -> Result<()> {
-    //     let xot = Xot::new();
-    //     let context = Context::new(&xot);
-    //     let xpath = CompiledXPath::new(&context, "1 + 2");
-
-    //     let result = xpath.interpret()?;
-    //     assert_eq!(as_integer(&result), 3);
-    //     Ok(())
-    // }
+    #[test]
+    fn test_compile_add() -> Result<()> {
+        let xot = Xot::new();
+        let context = Context::new(&xot);
+        let xpath = CompiledXPath::new(&context, "1 + 2");
+        let result = xpath.interpret()?;
+        assert_eq!(as_integer(&result), 3);
+        Ok(())
+    }
 }
