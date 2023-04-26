d44553bcf919cf1a419b0d90a8fcbaf2321937c5
Martijn Faassen <faassen@startifact.com>
2023-04-26 11:08:00 +0200

    Quantified false

--- xee-xpath/src/ir_interpret.rs
+++ xee-xpath/src/ir_interpret.rs
@@ -683,4 +683,9 @@ mod tests {
     fn test_some_quantifier_expr_true() {
         assert_debug_snapshot!(run("some $x in (1, 2, 3) satisfies $x eq 2"));
     }
+
+    #[test]
+    fn test_some_quantifier_expr_false() {
+        assert_debug_snapshot!(run("some $x in (1, 2, 3) satisfies $x eq 5"));
+    }
 }
