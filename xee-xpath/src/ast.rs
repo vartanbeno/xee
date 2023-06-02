9f7e92225cb377de5082ef8c9b44051754687ae1
Martijn Faassen <faassen@startifact.com>
2023-06-02 07:38:33 +0200

    Fix clippy warnings about module inception

--- xee-xpath/src/ast.rs
+++ xee-xpath/src/ast.rs
@@ -1,8 +1,8 @@
-mod ast;
+mod ast_core;
 mod parse_ast;
 mod rename;
 mod visitor;
 
-pub use ast::Name;
-pub(crate) use ast::*;
+pub use ast_core::Name;
+pub(crate) use ast_core::*;
 pub(crate) use parse_ast::{parse_expr_single, parse_xpath};
