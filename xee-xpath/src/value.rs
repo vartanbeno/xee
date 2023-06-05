63ff199db45df7d6aa3bb8272cf479b08a243a5f
Martijn Faassen <faassen@startifact.com>
2023-06-05 14:14:44 +0200

    Factor out Sequence into its own module.

--- xee-xpath/src/value.rs
+++ xee-xpath/src/value.rs
@@ -1,9 +1,11 @@
 mod atomic;
 mod error;
 mod node;
+mod sequence;
 mod value;
 
 pub use atomic::Atomic;
 pub use error::ValueError;
 pub use node::Node;
+pub use sequence::Sequence;
 pub use value::*;
