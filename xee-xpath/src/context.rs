644c90af088ba515a50c2e1d5ba1f38611e3f0e4
Martijn Faassen <faassen@startifact.com>
2023-06-08 10:52:31 +0200

    Separate out static function from static context into its own module

--- xee-xpath/src/context.rs
+++ xee-xpath/src/context.rs
@@ -1,7 +1,7 @@
 mod dynamic_context;
 mod static_context;
+mod static_function;
 
 pub use dynamic_context::DynamicContext;
-pub(crate) use static_context::ContextRule;
 pub use static_context::StaticContext;
-pub(crate) use static_context::{FunctionType, StaticFunctionDescription};
+pub(crate) use static_function::{ContextRule, FunctionType, StaticFunctionDescription};
