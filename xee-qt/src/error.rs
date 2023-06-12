e5f051e25d29757d7a257741745be01d87c64c79
Martijn Faassen <faassen@startifact.com>
2023-06-12 13:05:11 +0200

    Rename error enums

--- xee-qt/src/error.rs
+++ xee-qt/src/error.rs
@@ -3,30 +3,30 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Xee XPath error")]
-    XeeXPathError(xee_xpath::Error),
+    XeeXPath(xee_xpath::Error),
     #[error("Xot error")]
-    XotError(xot::Error),
+    Xot(xot::Error),
     #[error("IO error")]
-    IOError(std::io::Error),
+    IO(std::io::Error),
 }
 
 // turn any IO error into Error
 impl From<std::io::Error> for Error {
     fn from(err: std::io::Error) -> Self {
-        Error::IOError(err)
+        Error::IO(err)
     }
 }
 
 // turn any Xot error into Error
 impl From<xot::Error> for Error {
     fn from(err: xot::Error) -> Self {
-        Error::XotError(err)
+        Error::Xot(err)
     }
 }
 
 // turn any Xee XPath error into Error
 impl From<xee_xpath::Error> for Error {
     fn from(err: xee_xpath::Error) -> Self {
-        Error::XeeXPathError(err)
+        Error::XeeXPath(err)
     }
 }
