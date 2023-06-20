fd1c1c906a5ef5385ebff011cc80b426f2ac0433
Martijn Faassen <faassen@startifact.com>
2023-06-20 11:12:44 +0200

    Document it a little.

--- xee-xpath/src/occurrence.rs
+++ xee-xpath/src/occurrence.rs
@@ -1,5 +1,6 @@
 use std::result::Result;
 
+/// An occurrence API for an iterator that does not return results.
 pub trait Occurrence<A, E>: Iterator<Item = A> {
     fn one(&mut self) -> Result<A, E> {
         if let Some(one) = self.next() {
@@ -30,6 +31,7 @@ pub trait Occurrence<A, E>: Iterator<Item = A> {
     fn error(&self) -> E;
 }
 
+/// An occurrence API for an iterator that returns results.
 pub trait ResultOccurrence<A, E>: Iterator<Item = Result<A, E>> {
     fn one(&mut self) -> Result<A, E> {
         if let Some(one) = self.next() {
@@ -61,5 +63,3 @@ pub trait ResultOccurrence<A, E>: Iterator<Item = Result<A, E>> {
 
     fn error(&self) -> E;
 }
-
-// impl<A, I> Occurrence<A> for I where I: Iterator<Item = A> {}
