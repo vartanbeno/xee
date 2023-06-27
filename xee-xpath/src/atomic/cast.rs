b76e701556c7f8973e60339bc9d2fb8ec5b7a31a
Martijn Faassen <faassen@startifact.com>
2023-06-27 15:30:35 +0200

    More casting behavior.

--- xee-xpath/src/atomic/cast.rs
+++ xee-xpath/src/atomic/cast.rs
@@ -64,6 +64,14 @@ impl atomic::Atomic {
             .map_err(|_| error::Error::FORG0001)
     }
 
+    pub(crate) fn parse_boolean(s: &str) -> error::Result<atomic::Atomic> {
+        match s {
+            "true" => Ok(atomic::Atomic::Boolean(true)),
+            "false" => Ok(atomic::Atomic::Boolean(false)),
+            _ => Err(error::Error::FORG0001),
+        }
+    }
+
     // we can't use lexical::parse_float_options::XML as it doesn't allow INF
     // which is allowed by the XML Schema spec
 
@@ -137,15 +145,25 @@ impl atomic::Atomic {
         match self {
             atomic::Atomic::Float(_) => Ok(self.clone()),
             // TODO: this should implement the rule in 19.1.2.1
-            // https://www.w3.org/TR/xpath-functions-31/#casting-to-numerics
             atomic::Atomic::Double(OrderedFloat(d)) => {
+                // https://www.w3.org/TR/xpath-functions-31/#casting-to-numerics
+                // specifies a complex rule that involves the ranges for e of
+                // -149 to 104. As far as I can tell Rust uses a different
+                // range, -125 to 128. Both refer to IEEE 754-2008, so I'm
+                // confused. For now I'm keeping the simple implementation
+                // below, which may not behave exactly as per the conversion
+                // rules at the extremes of the ranges.
                 Ok(atomic::Atomic::Float(OrderedFloat(*d as f32)))
             }
-            atomic::Atomic::Decimal(_) => {
-                // TODO specification says to cast to string first, then
-                // from that to float
-                todo!();
-            }
+            atomic::Atomic::Decimal(_)
+            | atomic::Atomic::Integer(_)
+            | atomic::Atomic::Int(_)
+            | atomic::Atomic::Short(_)
+            | atomic::Atomic::Byte(_)
+            | atomic::Atomic::UnsignedLong(_)
+            | atomic::Atomic::UnsignedInt(_)
+            | atomic::Atomic::UnsignedShort(_)
+            | atomic::Atomic::UnsignedByte(_) => Self::parse_float(&self.to_canonical()),
             // TODO: any type of integer needs to cast to string first,
             // then to that from float
             atomic::Atomic::Boolean(b) => {
@@ -155,8 +173,150 @@ impl atomic::Atomic {
                     Ok(atomic::Atomic::Float(OrderedFloat(0.0)))
                 }
             }
+            atomic::Atomic::String(s) => Self::parse_float(s),
+            atomic::Atomic::Untyped(s) => Self::parse_float(s),
             _ => {
-                todo!();
+                panic!("absent not supported")
+            }
+        }
+    }
+
+    pub(crate) fn cast_to_double(&self) -> error::Result<atomic::Atomic> {
+        match self {
+            atomic::Atomic::Double(_) => Ok(self.clone()),
+            atomic::Atomic::Float(OrderedFloat(f)) => {
+                Ok(atomic::Atomic::Double(OrderedFloat(*f as f64)))
+            }
+            atomic::Atomic::Decimal(_)
+            | atomic::Atomic::Integer(_)
+            | atomic::Atomic::Int(_)
+            | atomic::Atomic::Short(_)
+            | atomic::Atomic::Byte(_)
+            | atomic::Atomic::UnsignedLong(_)
+            | atomic::Atomic::UnsignedInt(_)
+            | atomic::Atomic::UnsignedShort(_)
+            | atomic::Atomic::UnsignedByte(_) => Self::parse_double(&self.to_canonical()),
+            atomic::Atomic::Boolean(b) => {
+                if *b {
+                    Ok(atomic::Atomic::Double(OrderedFloat(1.0)))
+                } else {
+                    Ok(atomic::Atomic::Double(OrderedFloat(0.0)))
+                }
+            }
+            atomic::Atomic::String(s) => Self::parse_double(s),
+            atomic::Atomic::Untyped(s) => Self::parse_double(s),
+            _ => {
+                panic!("absent not supported")
+            }
+        }
+    }
+
+    pub(crate) fn cast_to_decimal(&self) -> error::Result<atomic::Atomic> {
+        match self {
+            atomic::Atomic::Decimal(_) => Ok(self.clone()),
+            atomic::Atomic::Integer(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::Int(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::Short(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::Byte(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::UnsignedLong(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::UnsignedInt(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::UnsignedShort(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::UnsignedByte(i) => Ok(atomic::Atomic::Decimal(Decimal::from(*i))),
+            atomic::Atomic::Float(OrderedFloat(f)) => {
+                if f.is_nan() || f.is_infinite() {
+                    return Err(error::Error::FOCA0002);
+                }
+
+                Ok(atomic::Atomic::Decimal(
+                    Decimal::try_from(*f).map_err(|_| error::Error::FOCA0001)?,
+                ))
+            }
+            atomic::Atomic::Double(OrderedFloat(f)) => {
+                if f.is_nan() || f.is_infinite() {
+                    return Err(error::Error::FOCA0002);
+                }
+
+                Ok(atomic::Atomic::Decimal(
+                    Decimal::try_from(*f).map_err(|_| error::Error::FOCA0001)?,
+                ))
+            }
+            atomic::Atomic::Boolean(b) => {
+                if *b {
+                    Ok(atomic::Atomic::Decimal(Decimal::from(1)))
+                } else {
+                    Ok(atomic::Atomic::Decimal(Decimal::from(0)))
+                }
+            }
+            atomic::Atomic::String(s) => Self::parse_decimal(s),
+            atomic::Atomic::Untyped(s) => Self::parse_decimal(s),
+            _ => {
+                panic!("absent not supported")
+            }
+        }
+    }
+
+    pub(crate) fn cast_to_integer(&self) -> error::Result<atomic::Atomic> {
+        match self {
+            atomic::Atomic::Integer(_)
+            | atomic::Atomic::Int(_)
+            | atomic::Atomic::Short(_)
+            | atomic::Atomic::Byte(_)
+            | atomic::Atomic::UnsignedLong(_)
+            | atomic::Atomic::UnsignedInt(_)
+            | atomic::Atomic::UnsignedShort(_)
+            | atomic::Atomic::UnsignedByte(_) => Ok(self.clone()),
+            atomic::Atomic::Decimal(d) => d
+                .trunc()
+                .try_into()
+                .map(atomic::Atomic::Integer)
+                .map_err(|_| error::Error::FOCA0003),
+            atomic::Atomic::Float(OrderedFloat(f)) => {
+                if f.is_nan() | f.is_infinite() {
+                    return Err(error::Error::FOCA0002);
+                }
+                let i: i64 = f.trunc().to_i64().ok_or(error::Error::FOCA0003)?;
+                Ok(atomic::Atomic::Integer(i))
+            }
+            atomic::Atomic::Double(OrderedFloat(d)) => {
+                if d.is_nan() | d.is_infinite() {
+                    return Err(error::Error::FOCA0002);
+                }
+                let i: i64 = d.trunc().to_i64().ok_or(error::Error::FOCA0003)?;
+                Ok(atomic::Atomic::Integer(i))
+            }
+            atomic::Atomic::Boolean(b) => {
+                if *b {
+                    Ok(atomic::Atomic::Integer(1))
+                } else {
+                    Ok(atomic::Atomic::Integer(0))
+                }
+            }
+            atomic::Atomic::String(s) => Self::parse_integer(s),
+            atomic::Atomic::Untyped(s) => Self::parse_integer(s),
+            atomic::Atomic::Absent => {
+                panic!("absent not supported")
+            }
+        }
+    }
+
+    pub(crate) fn cast_to_boolean(&self) -> error::Result<atomic::Atomic> {
+        match self {
+            atomic::Atomic::Boolean(_) => Ok(self.clone()),
+            atomic::Atomic::Float(f) => Ok(atomic::Atomic::Boolean(!(f.is_nan() || f.is_zero()))),
+            atomic::Atomic::Double(d) => Ok(atomic::Atomic::Boolean(!(d.is_nan() || d.is_zero()))),
+            atomic::Atomic::Decimal(d) => Ok(atomic::Atomic::Boolean(!d.is_zero())),
+            atomic::Atomic::Integer(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::Int(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::Short(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::Byte(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::UnsignedLong(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::UnsignedInt(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::UnsignedShort(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::UnsignedByte(i) => Ok(atomic::Atomic::Boolean(!i.is_zero())),
+            atomic::Atomic::String(s) => Self::parse_boolean(s),
+            atomic::Atomic::Untyped(s) => Self::parse_boolean(s),
+            _ => {
+                panic!("absent not supported")
             }
         }
     }
@@ -380,6 +540,121 @@ mod tests {
         assert_eq!(
             atomic::Atomic::Double(OrderedFloat(15.5)).to_canonical(),
             "15.5"
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_inf() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(f64::INFINITY))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(f32::INFINITY))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_negative_inf() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(f64::NEG_INFINITY))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(f32::NEG_INFINITY))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_nan() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(f64::NAN))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(f32::NAN))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_zero() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(0.0))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(0.0))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_negative_zero() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(-0.0))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(-0.0))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_too_big() {
+        // TODO: are the boundaries exactly as in spec?
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(f32::MAX as f64 * 1.1))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(f32::INFINITY))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_too_small() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(1e-150))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(0.0))
+        );
+    }
+
+    #[test]
+    fn test_cast_double_to_float_not_too_small() {
+        assert_eq!(
+            atomic::Atomic::Double(OrderedFloat(1e-5))
+                .cast_to_float()
+                .unwrap(),
+            atomic::Atomic::Float(OrderedFloat(1e-5))
+        );
+    }
+
+    #[test]
+    fn test_cast_int_to_decimal() {
+        assert_eq!(
+            atomic::Atomic::Int(15).cast_to_decimal().unwrap(),
+            atomic::Atomic::Decimal(dec!(15))
+        );
+    }
+
+    #[test]
+    fn test_cast_float_to_decimal() {
+        assert_eq!(
+            atomic::Atomic::Float(OrderedFloat(15.5))
+                .cast_to_decimal()
+                .unwrap(),
+            atomic::Atomic::Decimal(dec!(15.5))
+        );
+    }
+
+    #[test]
+    fn test_cast_inf_float_to_decimal() {
+        assert_eq!(
+            atomic::Atomic::Float(OrderedFloat(f32::INFINITY)).cast_to_decimal(),
+            Err(error::Error::FOCA0002)
+        );
+    }
+
+    #[test]
+    fn test_cast_huge_float_to_decimal() {
+        assert_eq!(
+            atomic::Atomic::Float(OrderedFloat(1.0e30)).cast_to_decimal(),
+            Err(error::Error::FOCA0001)
         );
     }
 }
