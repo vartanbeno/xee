bbcb55d727b9806f74f5c27cf7361f8eee18e2d3
Martijn Faassen <faassen@startifact.com>
2023-06-30 16:02:22 +0200

    Implementation of xs constructor functions based on casting.

--- xee-xpath/src/func/xs.rs
+++ xee-xpath/src/func/xs.rs
@@ -5,27 +5,14 @@ use crate::context::StaticFunctionDescription;
 use crate::error;
 use crate::wrap_xpath_fn;
 
-#[xpath_fn("xs:string($arg as xs:anyAtomicType?) as xs:string")]
-fn xs_string(arg: Option<atomic::Atomic>) -> error::Result<String> {
-    if let Some(arg) = arg {
-        arg.string_value()
-    } else {
-        Ok("".to_string())
-    }
+#[xpath_fn("xs:string($arg as xs:anyAtomicType?) as xs:string?")]
+fn xs_string(arg: Option<atomic::Atomic>) -> error::Result<Option<atomic::Atomic>> {
+    Ok(arg.map(|arg| arg.cast_to_string()))
 }
 
 #[xpath_fn("xs:int($arg as xs:anyAtomicType?) as xs:int?")]
-fn xs_int(arg: Option<atomic::Atomic>) -> error::Result<Option<i64>> {
-    if let Some(arg) = arg {
-        // TODO: only handle the string case for now
-        Ok(Some(
-            arg.string_value()?
-                .parse()
-                .map_err(|_| error::Error::FORG0001)?,
-        ))
-    } else {
-        Ok(None)
-    }
+fn xs_int(arg: Option<atomic::Atomic>) -> error::Result<Option<atomic::Atomic>> {
+    arg.map(|arg| arg.cast_to_int()).transpose()
 }
 
 pub(crate) fn static_function_descriptions() -> Vec<StaticFunctionDescription> {
