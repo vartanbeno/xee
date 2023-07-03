6f1a02c56e2e21419770592702dcf9d4ab7b5c81
Martijn Faassen <faassen@startifact.com>
2023-07-03 13:15:10 +0200

    We can reenable these tests.

--- xee-xpath/tests/qt3.rs
+++ xee-xpath/tests/qt3.rs
@@ -9,8 +9,6 @@ fn test_root() {
 fn test_if_expr() {
     Tests::all("prod/IfExpr")
         .exclude("CondExpr017 CondExpr018 CondExpr022 K-CondExpr-3 K-CondExpr-4 K-CondExpr-5 K-CondExpr-6 K-CondExpr-7 K-CondExpr-8 K-CondExpr-9 K-CondExpr-10 K-CondExpr-11 K-CondExpr-12 cbcl-*")
-        // use cast as
-        .exclude("CondExpr20 CondExpr21 CondExpr022")
         .run()
 }
 
