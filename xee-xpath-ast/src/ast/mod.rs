b7a74f147afaf79be49626f66249d725ca112d9e
Martijn Faassen <faassen@startifact.com>
2023-07-13 16:47:34 +0200

    Rename to parser; temporarily break snapshots as we're moving them.

--- xee-xpath-ast/src/ast/mod.rs
+++ xee-xpath-ast/src/ast/mod.rs
@@ -1,9 +1,9 @@
 mod ast_core;
-mod parse3;
+mod parser;
 mod rename;
 mod visitor;
 
 pub use ast_core::*;
-pub use parse3::{
+pub use parser::{
     parse_expr_single, parse_kind_test, parse_sequence_type, parse_signature, parse_xpath,
 };
