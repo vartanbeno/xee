6d633325474ad34cc1634a88e783cb28b598b453
Martijn Faassen <faassen@startifact.com>
2023-09-04 10:15:52 +0200

    Lots of date/time related functions.

--- xee-xpath/src/func/datetime.rs
+++ xee-xpath/src/func/datetime.rs
@@ -1,5 +1,8 @@
 // https://www.w3.org/TR/xpath-functions-31/#dates-times
 
+use chrono::{Datelike, SubsecRound, Timelike};
+use ibig::IBig;
+use rust_decimal::Decimal;
 use xee_xpath_macros::xpath_fn;
 
 use crate::context::StaticFunctionDescription;
@@ -37,6 +40,155 @@ fn date_time(
     }
 }
 
+#[xpath_fn("fn:year-from-dateTime($arg as xs:dateTime?) as xs:integer?")]
+fn year_from_date_time(arg: Option<NaiveDateTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date_time.year().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:month-from-dateTime($arg as xs:dateTime?) as xs:integer?")]
+fn month_from_date_time(arg: Option<NaiveDateTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date_time.month().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:day-from-dateTime($arg as xs:dateTime?) as xs:integer?")]
+fn day_from_date_time(arg: Option<NaiveDateTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date_time.day().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:hours-from-dateTime($arg as xs:dateTime?) as xs:integer?")]
+fn hours_from_date_time(arg: Option<NaiveDateTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date_time.hour().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:minutes-from-dateTime($arg as xs:dateTime?) as xs:integer?")]
+fn minutes_from_date_time(arg: Option<NaiveDateTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date_time.minute().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:seconds-from-dateTime($arg as xs:dateTime?) as xs:decimal?")]
+fn seconds_from_date_time(arg: Option<NaiveDateTimeWithOffset>) -> error::Result<Option<Decimal>> {
+    match arg {
+        Some(arg) => Ok(Some(seconds(arg.date_time))),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:timezone-from-dateTime($arg as xs:dateTime?) as xs:dayTimeDuration?")]
+fn timezone_from_date_time(
+    arg: Option<NaiveDateTimeWithOffset>,
+) -> error::Result<Option<chrono::Duration>> {
+    match arg {
+        Some(arg) => Ok(duration(arg.offset)),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:year-from-date($arg as xs:date?) as xs:integer?")]
+fn year_from_date(arg: Option<NaiveDateWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date.year().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:month-from-date($arg as xs:date?) as xs:integer?")]
+fn month_from_date(arg: Option<NaiveDateWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date.month().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:day-from-date($arg as xs:date?) as xs:integer?")]
+fn day_from_date(arg: Option<NaiveDateWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.date.day().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:timezone-from-date($arg as xs:date?) as xs:time?")]
+fn timezone_from_date(arg: Option<NaiveDateWithOffset>) -> error::Result<Option<chrono::Duration>> {
+    match arg {
+        Some(arg) => Ok(duration(arg.offset)),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:hours-from-time($arg as xs:time?) as xs:integer?")]
+fn hours_from_time(arg: Option<NaiveTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.time.hour().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:minutes-from-time($arg as xs:time?) as xs:integer?")]
+fn minutes_from_time(arg: Option<NaiveTimeWithOffset>) -> error::Result<Option<IBig>> {
+    match arg {
+        Some(arg) => Ok(Some(arg.time.minute().into())),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:seconds-from-time($arg as xs:time?) as xs:decimal?")]
+fn seconds_from_time(arg: Option<NaiveTimeWithOffset>) -> error::Result<Option<Decimal>> {
+    match arg {
+        Some(arg) => Ok(Some(seconds(arg.time))),
+        None => Ok(None),
+    }
+}
+
+#[xpath_fn("fn:timezone-from-time($arg as xs:time?) as xs:dayTimeDuration?")]
+fn timezone_from_time(arg: Option<NaiveTimeWithOffset>) -> error::Result<Option<chrono::Duration>> {
+    match arg {
+        Some(arg) => Ok(duration(arg.offset)),
+        None => Ok(None),
+    }
+}
+
+fn seconds(time: impl Timelike + SubsecRound + Copy) -> Decimal {
+    let nanoseconds: Decimal = time.round_subsecs(3).nanosecond().into();
+    let seconds: Decimal = time.second().into();
+    seconds + (nanoseconds / Decimal::from(1_000_000_000))
+}
+
+fn duration(offset: Option<chrono::FixedOffset>) -> Option<chrono::Duration> {
+    offset.map(|offset| chrono::Duration::seconds(offset.local_minus_utc() as i64))
+}
+
 pub(crate) fn static_function_descriptions() -> Vec<StaticFunctionDescription> {
-    vec![wrap_xpath_fn!(date_time)]
+    vec![
+        wrap_xpath_fn!(date_time),
+        wrap_xpath_fn!(year_from_date_time),
+        wrap_xpath_fn!(month_from_date_time),
+        wrap_xpath_fn!(day_from_date_time),
+        wrap_xpath_fn!(hours_from_date_time),
+        wrap_xpath_fn!(minutes_from_date_time),
+        wrap_xpath_fn!(seconds_from_date_time),
+        wrap_xpath_fn!(timezone_from_date_time),
+        wrap_xpath_fn!(year_from_date),
+        wrap_xpath_fn!(month_from_date),
+        wrap_xpath_fn!(day_from_date),
+        wrap_xpath_fn!(timezone_from_date),
+        wrap_xpath_fn!(hours_from_time),
+        wrap_xpath_fn!(minutes_from_time),
+        wrap_xpath_fn!(seconds_from_time),
+        wrap_xpath_fn!(timezone_from_time),
+    ]
 }
