16318a548e92bc28fbf7ffd7fd27dda3bc9a38ab
Martijn Faassen <faassen@startifact.com>
2024-02-05 14:59:13 +0100

    Dealing with descendant axis.

--- xee-interpreter/src/pattern/pattern_core2.rs
+++ xee-interpreter/src/pattern/pattern_core2.rs
@@ -9,6 +9,12 @@ struct Patterns<V> {
     patterns: Vec<(pattern::Pattern, V)>,
 }
 
+enum Backwards {
+    NotFound,
+    One,
+    Any,
+}
+
 impl<V> Patterns<V> {
     pub(crate) fn lookup(&self, item: &Item, xot: &Xot) -> Option<&V> {
         for (pattern, value) in &self.patterns {
@@ -65,20 +71,43 @@ impl<V> Patterns<V> {
         xot: &Xot,
     ) -> bool {
         let mut node = Some(node);
+        let mut backwards = Backwards::One;
         for step in steps.iter().rev() {
-            if let Some(n) = node {
-                if !self.matches_step_expr(step, n, xot) {
-                    return false;
+            match backwards {
+                Backwards::NotFound => return false,
+                Backwards::One => {
+                    if let Some(n) = node {
+                        backwards = self.matches_step_expr(step, n, xot);
+                        node = n.parent(xot);
+                    } else {
+                        return false;
+                    }
                 }
-                node = n.parent(xot);
-            } else {
-                return false;
+                Backwards::Any => loop {
+                    if let Some(n) = node {
+                        let new_backwards = self.matches_step_expr(step, n, xot);
+                        match new_backwards {
+                            Backwards::NotFound => {
+                                // this parent wasn't it, so go up one more
+                                node = n.parent(xot);
+                            }
+                            // we did find it
+                            _ => {
+                                backwards = new_backwards;
+                                node = n.parent(xot);
+                                break;
+                            }
+                        }
+                    } else {
+                        return false;
+                    }
+                },
             }
         }
-        true
+        !matches!(backwards, Backwards::NotFound)
     }
 
-    fn matches_step_expr(&self, step: &pattern::StepExpr, node: xml::Node, xot: &Xot) -> bool {
+    fn matches_step_expr(&self, step: &pattern::StepExpr, node: xml::Node, xot: &Xot) -> Backwards {
         match node {
             xml::Node::Xot(node) => match step {
                 pattern::StepExpr::AxisStep(axis_step) => {
@@ -91,14 +120,30 @@ impl<V> Patterns<V> {
         }
     }
 
-    fn matches_axis_step(&self, step: &pattern::AxisStep, node: xot::Node, xot: &Xot) -> bool {
-        if step.forward != pattern::ForwardAxis::Child {
-            todo!();
-        }
+    fn matches_axis_step(&self, step: &pattern::AxisStep, node: xot::Node, xot: &Xot) -> Backwards {
         if !step.predicates.is_empty() {
             todo!();
         }
-        self.matches_node_test(&step.node_test, node, xot)
+        match &step.forward {
+            pattern::ForwardAxis::Child => {
+                if self.matches_node_test(&step.node_test, node, xot) {
+                    Backwards::One
+                } else {
+                    Backwards::NotFound
+                }
+            }
+            pattern::ForwardAxis::Descendant => {
+                if self.matches_node_test(&step.node_test, node, xot) {
+                    Backwards::Any
+                } else {
+                    Backwards::NotFound
+                }
+            }
+            pattern::ForwardAxis::Attribute => todo!(),
+            pattern::ForwardAxis::Self_ => todo!(),
+            pattern::ForwardAxis::DescendantOrSelf => todo!(),
+            pattern::ForwardAxis::Namespace => todo!(),
+        }
     }
 
     fn matches_node_test(&self, node_test: &pattern::NodeTest, node: xot::Node, xot: &Xot) -> bool {
@@ -147,6 +192,25 @@ mod tests {
     }
 
     #[test]
+    fn test_match_name_not_found() {
+        let mut xot = Xot::new();
+        let root = xot.parse(r#"<root><foo/></root>"#).unwrap();
+        let document_element = xot.document_element(root).unwrap();
+        let node = xot.first_child(document_element).unwrap();
+        let node = xml::Node::Xot(node);
+        let item: Item = node.into();
+
+        let mut patterns = Patterns {
+            patterns: Vec::new(),
+        };
+        let namespaces = xee_name::Namespaces::default();
+        let variable_names = xee_name::VariableNames::default();
+        let pattern = pattern::Pattern::parse("notfound", &namespaces, &variable_names).unwrap();
+        patterns.patterns.push((pattern, 1));
+        assert_eq!(patterns.lookup(&item, &xot), None);
+    }
+
+    #[test]
     fn test_match_name_nested() {
         let mut xot = Xot::new();
         let root = xot.parse(r#"<root><bar><foo/></bar></root>"#).unwrap();
@@ -165,5 +229,120 @@ mod tests {
         patterns.patterns.push((pattern, 1));
         let found = patterns.lookup(&item, &xot).unwrap();
         assert_eq!(*found, 1);
+    }
+
+    #[test]
+    fn test_match_name_nested_notfound() {
+        let mut xot = Xot::new();
+        let root = xot.parse(r#"<root><bar><foo/></bar></root>"#).unwrap();
+        let document_element = xot.document_element(root).unwrap();
+        let node = xot.first_child(document_element).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xml::Node::Xot(node);
+        let item: Item = node.into();
+
+        let mut patterns = Patterns {
+            patterns: Vec::new(),
+        };
+        let namespaces = xee_name::Namespaces::default();
+        let variable_names = xee_name::VariableNames::default();
+        let pattern =
+            pattern::Pattern::parse("notfound/foo", &namespaces, &variable_names).unwrap();
+        patterns.patterns.push((pattern, 1));
+        assert_eq!(patterns.lookup(&item, &xot), None);
+    }
+
+    #[test]
+    fn test_match_name_nested_with_explicit_descendant_axis() {
+        let mut xot = Xot::new();
+        let root = xot.parse(r#"<root><bar><foo/></bar></root>"#).unwrap();
+        let document_element = xot.document_element(root).unwrap();
+        let node = xot.first_child(document_element).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xml::Node::Xot(node);
+        let item: Item = node.into();
+
+        let mut patterns = Patterns {
+            patterns: Vec::new(),
+        };
+        let namespaces = xee_name::Namespaces::default();
+        let variable_names = xee_name::VariableNames::default();
+        let pattern =
+            pattern::Pattern::parse("bar/descendant::foo", &namespaces, &variable_names).unwrap();
+        patterns.patterns.push((pattern, 1));
+        let found = patterns.lookup(&item, &xot).unwrap();
+        assert_eq!(*found, 1);
+    }
+
+    #[test]
+    fn test_match_name_nested_with_explicit_descendant_axis_not_found() {
+        let mut xot = Xot::new();
+        let root = xot.parse(r#"<root><bar><foo/></bar></root>"#).unwrap();
+        let document_element = xot.document_element(root).unwrap();
+        let node = xot.first_child(document_element).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xml::Node::Xot(node);
+        let item: Item = node.into();
+
+        let mut patterns = Patterns {
+            patterns: Vec::new(),
+        };
+        let namespaces = xee_name::Namespaces::default();
+        let variable_names = xee_name::VariableNames::default();
+        let pattern =
+            pattern::Pattern::parse("notfound/descendant::foo", &namespaces, &variable_names)
+                .unwrap();
+        patterns.patterns.push((pattern, 1));
+        assert_eq!(patterns.lookup(&item, &xot), None);
+    }
+
+    #[test]
+    fn test_match_name_nested_actually_with_explicit_descendant_axis() {
+        let mut xot = Xot::new();
+        let root = xot
+            .parse(r#"<root><qux><bar><foo/></bar></qux></root>"#)
+            .unwrap();
+        let document_element = xot.document_element(root).unwrap();
+        let node = xot.first_child(document_element).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xml::Node::Xot(node);
+        let item: Item = node.into();
+
+        let mut patterns = Patterns {
+            patterns: Vec::new(),
+        };
+        let namespaces = xee_name::Namespaces::default();
+        let variable_names = xee_name::VariableNames::default();
+        let pattern =
+            pattern::Pattern::parse("qux/descendant::foo", &namespaces, &variable_names).unwrap();
+        patterns.patterns.push((pattern, 1));
+        let found = patterns.lookup(&item, &xot).unwrap();
+        assert_eq!(*found, 1);
+    }
+
+    #[test]
+    fn test_match_name_nested_actually_with_explicit_descendant_axis_not_found() {
+        let mut xot = Xot::new();
+        let root = xot
+            .parse(r#"<root><qux><bar><foo/></bar></qux></root>"#)
+            .unwrap();
+        let document_element = xot.document_element(root).unwrap();
+        let node = xot.first_child(document_element).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xot.first_child(node).unwrap();
+        let node = xml::Node::Xot(node);
+        let item: Item = node.into();
+
+        let mut patterns = Patterns {
+            patterns: Vec::new(),
+        };
+        let namespaces = xee_name::Namespaces::default();
+        let variable_names = xee_name::VariableNames::default();
+        let pattern =
+            pattern::Pattern::parse("notfound/descendant::foo", &namespaces, &variable_names)
+                .unwrap();
+        patterns.patterns.push((pattern, 1));
+        assert_eq!(patterns.lookup(&item, &xot), None);
     }
 }
