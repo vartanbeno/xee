e0066ce326462ff2ed5ef64806a10a6a9af1c032
Martijn Faassen <faassen@startifact.com>
2024-02-21 17:14:15 +0100

    Rename the module.

--- xee-ir/src/lib.rs
+++ xee-ir/src/lib.rs
@@ -1,14 +1,14 @@
 mod binding;
 mod builder;
 mod compile;
+mod function_compiler;
 pub mod ir;
-mod ir_interpret;
 mod scope;
 mod variables;
 
 pub use binding::{Binding, Bindings};
 pub use builder::FunctionBuilder;
 pub use compile::{compile_xpath, compile_xslt};
-pub use ir_interpret::FunctionCompiler;
+pub use function_compiler::FunctionCompiler;
 pub use scope::Scopes;
 pub use variables::Variables;
