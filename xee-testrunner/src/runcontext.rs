d7b96869687f233c616f35ac0f3e92198be08d0e
Martijn Faassen <faassen@startifact.com>
2024-02-23 17:32:23 +0100

    Hook in dependencies.

--- xee-testrunner/src/runcontext.rs
+++ xee-testrunner/src/runcontext.rs
@@ -1,12 +1,12 @@
 use xee_xpath::xml::Documents;
 use xot::Xot;
 
-use crate::{catalog::Catalog, environment::Environment};
+use crate::{catalog::Catalog, dependency::KnownDependencies, environment::Environment};
 
 pub(crate) struct RunContext<E: Environment> {
     pub(crate) xot: Xot,
     pub(crate) catalog: Catalog<E>,
     pub(crate) documents: Documents,
-    // pub(crate) known_dependencies: KnownDependencies,
+    pub(crate) known_dependencies: KnownDependencies,
     // pub(crate) verbose: bool,
 }
