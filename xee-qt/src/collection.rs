eb3d99bc45554879137598a4d16ac66004818185
Martijn Faassen <faassen@startifact.com>
2024-02-28 15:46:24 +0100

    Remove old xee-qt in favor of xee-testrunner

--- xee-qt/src/collection.rs
+++
@@ -1,8 +0,0 @@
-use fxhash::FxBuildHasher;
-use indexmap::{IndexMap, IndexSet};
-
-// we use indexmap so we get stable serialization in insta tests,
-// The run-time impact should be small as we're not really
-// intense users
-pub(crate) type FxIndexMap<K, V> = IndexMap<K, V, FxBuildHasher>;
-pub(crate) type FxIndexSet<T> = IndexSet<T, FxBuildHasher>;
--- xee-qt/src/main.rs
+++
@@ -1,6 +0,0 @@
-use xee_qt::cli;
-use xee_qt::Result;
-
-fn main() -> Result<()> {
-    cli()
-}
