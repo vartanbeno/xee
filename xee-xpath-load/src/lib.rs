c8b0c262ce0c07acb318f8712ed7efd75d14d679
Martijn Faassen <faassen@startifact.com>
2024-03-11 15:24:35 +0100

    Start to extract xpath load functionality from xee-xpath.

---
+++ xee-xpath-load/src/lib.rs
@@ -0,0 +1,5 @@
+mod query;
+
+pub use crate::query::{
+    Convert, ManyQuery, OneQuery, OptionQuery, Queries, Query, Recurse, Session,
+};
--- xee-xpath/src/lib.rs
+++ xee-xpath/src/lib.rs
@@ -8,7 +8,7 @@ pub use xee_xpath_ast::ast::Name;
 pub use xee_xpath_ast::{Namespaces, VariableNames};
 
 pub use xee_interpreter::interpreter::Runnable;
-pub use xee_interpreter::{atomic, context, error, occurrence, sequence, string, xml};
+pub use xee_interpreter::{atomic, context, error, interpreter, occurrence, sequence, string, xml};
 
 pub use crate::ast_ir::IrConverter;
 pub use crate::compile::{compile, parse};
