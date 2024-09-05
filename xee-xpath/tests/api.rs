82aaa561a5b3738fbaf77b191658d1547d0558cb
Martijn Faassen <faassen@startifact.com>
2024-09-05 17:04:09 +0200

    Improve API.
    We have reached conclusions about what to do with error handling, as getting an error for absent is really
    making life harder.

--- xee-xpath/tests/api.rs
+++ xee-xpath/tests/api.rs
@@ -1,9 +1,9 @@
-use xee_xpath::{Documents, Engine, Item, Result, XPaths};
+use xee_xpath::{Documents, Engine, Item, Result, SpannedResult, XPaths};
 
 #[test]
-fn test_basic() {
+fn test_basic() -> SpannedResult<()> {
     let mut xpaths = XPaths::default();
-    let e = xpaths.compile("//root/string()").unwrap();
+    let e = xpaths.compile("//root/string()")?;
 
     let mut documents = Documents::new();
     let doc = documents
@@ -11,10 +11,11 @@ fn test_basic() {
         .unwrap();
 
     let mut engine = Engine::new(&xpaths, documents);
-    let result = engine.evaluate(e, doc).unwrap();
-    // we can collect this and we either have an error of we get an item
+    let result = engine.evaluate(e, doc)?;
+    // we can collect this and we either have an error or we get an item
     let result = result.into_iter().collect::<Result<Vec<Item>>>().unwrap();
 
     assert_eq!(result.len(), 1);
     assert_eq!(result[0].to_atomic().unwrap().to_string().unwrap(), "text");
+    Ok(())
 }
