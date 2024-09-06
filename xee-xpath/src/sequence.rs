dc25fb79dd2d05b49a807fcffdf7328d3f311e30
Martijn Faassen <faassen@startifact.com>
2024-09-06 13:17:05 +0200

    Test basic behavior.

--- xee-xpath/src/sequence.rs
+++ xee-xpath/src/sequence.rs
@@ -12,11 +12,16 @@ impl Sequence {
     }
 }
 
-// impl IntoIterator for Sequence {
-//     type Item = xee_interpreter::error::Result<Item>;
-//     type IntoIter = xee_interpreter::sequence::ItemIter;
+impl IntoIterator for Sequence {
+    type Item = Item;
+    type IntoIter = xee_interpreter::sequence::ItemIter;
 
-//     fn into_iter(self) -> Self::IntoIter {
-//         self.sequence.items()
-//     }
-// }
+    fn into_iter(self) -> Self::IntoIter {
+        // TODO: this unwrap is weird, but items can fail to be created
+        // if the items are absent. but if we ensure in our API that
+        // an absent Sequence is never created, we can remove this unwrap.
+        // That is, instead of items being a result we could make *sequence*
+        // fail if it would be created as absent
+        self.sequence.items().unwrap()
+    }
+}
