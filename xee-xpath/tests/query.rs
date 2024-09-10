9c44e52cd52ac1730002cd2c112c004c091ede17
Martijn Faassen <faassen@startifact.com>
2024-09-10 16:43:56 +0200

    Add option query.

--- xee-xpath/tests/query.rs
+++ xee-xpath/tests/query.rs
@@ -19,6 +19,32 @@ fn test_simple_query() -> error::Result<()> {
 }
 
 #[test]
+fn test_option_query() -> error::Result<()> {
+    let mut documents = Documents::new();
+    let doc_with_value = documents
+        .load_string(
+            "http://example.com/with_value",
+            "<root><value>Foo</value></root>",
+        )
+        .unwrap();
+    let doc_without_value = documents
+        .load_string("http://example.com/without_value", "<root></root>")
+        .unwrap();
+
+    let mut queries = Queries::default();
+    let q = queries.option("/root/value/string()", |_, item| {
+        Ok(item.try_into_value::<String>()?)
+    })?;
+
+    let mut session = queries.session(documents);
+    let r = q.execute(&mut session, doc_with_value)?;
+    assert_eq!(r, Some("Foo".to_string()));
+    let r = q.execute(&mut session, doc_without_value)?;
+    assert_eq!(r, None);
+    Ok(())
+}
+
+#[test]
 fn test_nested_query() -> error::Result<()> {
     let mut documents = Documents::new();
     let doc = documents
