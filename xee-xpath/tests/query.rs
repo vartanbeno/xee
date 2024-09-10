9b214d0f9d67420f74096807970426b3fc465c07
Martijn Faassen <faassen@startifact.com>
2024-09-10 16:36:48 +0200

    Better whitespace.

--- xee-xpath/tests/query.rs
+++ xee-xpath/tests/query.rs
@@ -6,6 +6,7 @@ fn test_simple_query() -> error::Result<()> {
     let doc = documents
         .load_string("http://example.com", "<root>foo</root>")
         .unwrap();
+
     let mut queries = Queries::default();
     let q = queries.one("/root/string()", |_, item| {
         Ok(item.try_into_value::<String>()?)
@@ -20,12 +21,11 @@ fn test_simple_query() -> error::Result<()> {
 #[test]
 fn test_nested_query() -> error::Result<()> {
     let mut documents = Documents::new();
-
     let doc = documents
         .load_string("http://example.com", "<root><a>1</a><a>2</a></root>")
         .unwrap();
-    let mut queries = Queries::default();
 
+    let mut queries = Queries::default();
     let f_query = queries.one("./number()", |_, item| Ok(item.try_into_value::<f64>()?))?;
     let q = queries.many("/root/a", |session, item| {
         Ok(f_query.execute(session, item)?)
