1aaaf529df32debeb583fb7a9f21082e947578ee
Martijn Faassen <faassen@startifact.com>
2024-09-26 17:19:45 +0200

    Finish renaming back to document again.

--- xee-interpreter/src/xml/mod.rs
+++ xee-interpreter/src/xml/mod.rs
@@ -1,13 +1,11 @@
 /// XML integration.
 mod annotation;
-mod document2;
+mod document;
 mod kind_test;
 mod step;
 
 pub use annotation::Annotations;
-// pub use document::Document;
-pub use document2::{Document, DocumentHandle, Documents, DocumentsError, Uri};
-// pub use document::{Documents, Uri};
+pub use document::{Document, DocumentHandle, Documents, DocumentsError, Uri};
 pub(crate) use kind_test::kind_test;
 pub(crate) use step::resolve_step;
 pub use step::Step;
