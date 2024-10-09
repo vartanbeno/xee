696bf0683fb092eb0e51fb5c2bbb1de52e3d7ef1
Martijn Faassen <faassen@startifact.com>
2024-10-09 13:22:24 +0200

    encode-for-uri

--- xee-interpreter/src/library/uri.rs
+++ xee-interpreter/src/library/uri.rs
@@ -14,6 +14,48 @@ const IRI: percent_encoding::AsciiSet = percent_encoding::CONTROLS
     .add(b'^')
     .add(b'`');
 
+// the only things not encoded are the upper and lower case letters, the
+// digits, '-', '_', '.' and '~'.
+const ENCODE_FOR_URI: percent_encoding::AsciiSet = percent_encoding::CONTROLS
+    .add(b' ')
+    .add(b'!')
+    .add(b'"')
+    .add(b'#')
+    .add(b'$')
+    .add(b'%')
+    .add(b'&')
+    .add(b'\'')
+    .add(b'(')
+    .add(b')')
+    .add(b'*')
+    .add(b'+')
+    .add(b',')
+    .add(b'/')
+    .add(b':')
+    .add(b';')
+    .add(b'<')
+    .add(b'=')
+    .add(b'>')
+    .add(b'?')
+    .add(b'@')
+    .add(b'[')
+    .add(b'\\')
+    .add(b']')
+    .add(b'^')
+    .add(b'`')
+    .add(b'{')
+    .add(b'|')
+    .add(b'}');
+
+#[xpath_fn("fn:encode-for-uri($uripart as xs:string?) as xs:string")]
+fn encode_for_uri(uripart: Option<&str>) -> String {
+    if let Some(uri_part) = uripart {
+        percent_encoding::utf8_percent_encode(uri_part, &ENCODE_FOR_URI).to_string()
+    } else {
+        "".to_string()
+    }
+}
+
 #[xpath_fn("fn:iri-to-uri($iri as xs:string?) as xs:string")]
 fn iri_to_uri(iri: Option<&str>) -> String {
     if let Some(iri) = iri {
@@ -33,5 +75,9 @@ fn escape_html_uri(uri: Option<&str>) -> String {
 }
 
 pub(crate) fn static_function_descriptions() -> Vec<StaticFunctionDescription> {
-    vec![wrap_xpath_fn!(iri_to_uri), wrap_xpath_fn!(escape_html_uri)]
+    vec![
+        wrap_xpath_fn!(encode_for_uri),
+        wrap_xpath_fn!(iri_to_uri),
+        wrap_xpath_fn!(escape_html_uri),
+    ]
 }
