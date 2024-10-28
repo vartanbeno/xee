935b7a71d9f3ac8429fe4756bea85cb22802d4ea
Martijn Faassen <faassen@startifact.com>
2024-10-28 15:10:57 +0100

    Nicer implementation now that it's possible.

--- xee-interpreter/src/library/fn_.rs
+++ xee-interpreter/src/library/fn_.rs
@@ -1,13 +1,10 @@
 use ibig::IBig;
 
-use xee_name::{Name, FN_NAMESPACE};
-use xee_xpath_ast::ast;
 use xee_xpath_macros::xpath_fn;
 
 use crate::context::DynamicContext;
 use crate::error;
 use crate::function::StaticFunctionDescription;
-use crate::interpreter;
 use crate::sequence;
 use crate::wrap_xpath_fn;
 
@@ -29,11 +26,10 @@ fn generate_id(context: &DynamicContext, arg: Option<xot::Node>) -> String {
     }
 }
 
-fn error(
-    _context: &DynamicContext,
-    _interpreter: &mut interpreter::Interpreter,
-    _arguments: &[sequence::Sequence],
-) -> error::Result<sequence::Sequence> {
+// FIXME: this is not the right signature for fn:error, as it always
+// returns a none. I'm not sure how well defined `none` is in the type system?
+#[xpath_fn("fn:error() as item()*")]
+fn error_zero_args() -> error::Result<sequence::Sequence> {
     Err(error::Error::FOER0000)
 }
 
@@ -41,15 +37,6 @@ pub(crate) fn static_function_descriptions() -> Vec<StaticFunctionDescription> {
     vec![
         wrap_xpath_fn!(my_function),
         wrap_xpath_fn!(generate_id),
-        StaticFunctionDescription {
-            name: Name::new("error".to_string(), FN_NAMESPACE.to_string(), String::new()),
-            // TODO: this is not the right signature for fn:error, as it always
-            // returns a none
-            signature: ast::Signature::parse("fn:error() as item()*", &Default::default())
-                .unwrap()
-                .into(),
-            function_kind: None,
-            func: error,
-        },
+        wrap_xpath_fn!(error_zero_args),
     ]
 }
