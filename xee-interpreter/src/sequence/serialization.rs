b27616224d06a52118f6a22fefe6de8d03bd2081
Martijn Faassen <faassen@startifact.com>
2024-11-04 16:50:19 +0100

    More macro usage for fun and profit.

--- xee-interpreter/src/sequence/serialization.rs
+++ xee-interpreter/src/sequence/serialization.rs
@@ -19,7 +19,7 @@ pub(crate) struct SerializationParameters {
     doctype_public: Option<String>,
     doctype_system: Option<String>,
     encoding: String,
-    escape_uri_attribtue: bool,
+    escape_uri_attributes: bool,
     html_version: Decimal,
     include_content_type: bool,
     indent: bool,
@@ -38,10 +38,16 @@ pub(crate) struct SerializationParameters {
 
 // this would be prettier with some fancy derive macro, but we don't
 // have that many functions that need this and this is easier to make.
-macro_rules! option_parameter_conversion_option {
-    ($map:ident, $xpath_name:literal, $occurrence:expr, $atomic:expr, $ty:ty, $default:literal, $static_context:ident, $xot:ident) => {{
+macro_rules! option_parameter_conversion_option_with_default {
+    ($map:ident, $xpath_name:literal, $atomic:expr, $ty:ty, $default:expr, $static_context:ident, $xot:ident) => {{
         let name: atomic::Atomic = $xpath_name.to_string().into();
-        let value = $map.get_as_type(&name, $occurrence, $atomic, $static_context, $xot)?;
+        let value = $map.get_as_type(
+            &name,
+            ast::Occurrence::Option,
+            $atomic,
+            $static_context,
+            $xot,
+        )?;
         let value = if let Some(value) = value {
             value.items()?.option()?
         } else {
@@ -56,16 +62,48 @@ macro_rules! option_parameter_conversion_option {
     }};
 }
 
+macro_rules! option_parameter_conversion_option {
+    ($map:ident, $xpath_name:literal, $atomic:expr, $ty:ty, $static_context:ident, $xot:ident) => {{
+        let name: atomic::Atomic = $xpath_name.to_string().into();
+        let value = $map.get_as_type(
+            &name,
+            ast::Occurrence::Option,
+            $atomic,
+            $static_context,
+            $xot,
+        )?;
+        let value = if let Some(value) = value {
+            value.items()?.option()?
+        } else {
+            None
+        };
+        let value: Option<$ty> = if let Some(value) = value {
+            Some(value.to_atomic()?.try_into()?)
+        } else {
+            None
+        };
+        value
+    }};
+}
+
 impl SerializationParameters {
     fn from_map(
         map: Map,
         static_context: &context::StaticContext,
         xot: &Xot,
     ) -> error::Result<Self> {
-        let allow_duplicate_names = option_parameter_conversion_option!(
+        let allow_duplicate_names = option_parameter_conversion_option_with_default!(
             map,
             "allow-duplicate-names",
-            ast::Occurrence::Option,
+            Xs::Boolean,
+            bool,
+            false,
+            static_context,
+            xot
+        );
+        let byte_order_mark = option_parameter_conversion_option_with_default!(
+            map,
+            "byte-order-mark",
             Xs::Boolean,
             bool,
             false,
@@ -73,47 +111,173 @@ impl SerializationParameters {
             xot
         );
 
-        // let allow_duplicate_names: atomic::Atomic = "allow-duplicate-names".to_string().into();
-        // let allow_duplicate_names = map.get_as_type(
-        //     &allow_duplicate_names,
-        //     ast::Occurrence::Option,
-        //     Xs::Boolean,
-        //     static_context,
-        //     xot,
-        // )?;
-        // let allow_duplicate_names = if let Some(allow_duplicate_names) = allow_duplicate_names {
-        //     allow_duplicate_names.items()?.option()?
-        // } else {
-        //     None
-        // };
-        // let allow_duplicate_names: bool = if let Some(allow_duplicate_names) = allow_duplicate_names
-        // {
-        //     allow_duplicate_names.to_atomic()?.try_into()?
-        // } else {
-        //     false
-        // };
+        // cdata-section-elements
+
+        let doctype_public = option_parameter_conversion_option!(
+            map,
+            "doctype-public",
+            Xs::String,
+            String,
+            static_context,
+            xot
+        );
+
+        let doctype_system = option_parameter_conversion_option!(
+            map,
+            "doctype-system",
+            Xs::String,
+            String,
+            static_context,
+            xot
+        );
+
+        let encoding = option_parameter_conversion_option_with_default!(
+            map,
+            "encoding",
+            Xs::String,
+            String,
+            "utf-8".to_string(),
+            static_context,
+            xot
+        );
+
+        let escape_uri_attributes = option_parameter_conversion_option_with_default!(
+            map,
+            "escape-uri-attributes",
+            Xs::Boolean,
+            bool,
+            true,
+            static_context,
+            xot
+        );
+
+        let html_version = option_parameter_conversion_option_with_default!(
+            map,
+            "html-version",
+            Xs::Decimal,
+            Decimal,
+            Decimal::from_str_exact("5.0").unwrap(),
+            static_context,
+            xot
+        );
+
+        let include_content_type = option_parameter_conversion_option_with_default!(
+            map,
+            "include-content-type",
+            Xs::Boolean,
+            bool,
+            true,
+            static_context,
+            xot
+        );
+
+        let indent = option_parameter_conversion_option_with_default!(
+            map,
+            "indent",
+            Xs::Boolean,
+            bool,
+            false,
+            static_context,
+            xot
+        );
+
+        let item_separator = option_parameter_conversion_option_with_default!(
+            map,
+            "item-separator",
+            Xs::String,
+            String,
+            " ".to_string(),
+            static_context,
+            xot
+        );
+
+        // json-node-output-method
+
+        let media_type = option_parameter_conversion_option!(
+            map,
+            "media-type",
+            Xs::String,
+            String,
+            static_context,
+            xot
+        );
+
+        // method
+
+        let normalization_form = option_parameter_conversion_option!(
+            map,
+            "normalization-form",
+            Xs::String,
+            String,
+            static_context,
+            xot
+        );
+
+        let omit_xml_declaration = option_parameter_conversion_option_with_default!(
+            map,
+            "omit-xml-declaration",
+            Xs::Boolean,
+            bool,
+            true,
+            static_context,
+            xot
+        );
+
+        let standalone = option_parameter_conversion_option!(
+            map,
+            "standalone",
+            Xs::Boolean,
+            bool,
+            static_context,
+            xot
+        );
+
+        // suppress-indentation
+
+        let undeclare_prefixes = option_parameter_conversion_option_with_default!(
+            map,
+            "undeclare-prefixes",
+            Xs::Boolean,
+            bool,
+            false,
+            static_context,
+            xot
+        );
+
+        // use-character-maps
+
+        let version = option_parameter_conversion_option_with_default!(
+            map,
+            "version",
+            Xs::String,
+            String,
+            "1.0".to_string(),
+            static_context,
+            xot
+        );
+
         Ok(Self {
             allow_duplicate_names,
-            byte_order_mark: false,
+            byte_order_mark,
             cdata_section_elements: Vec::new(),
-            doctype_public: None,
-            doctype_system: None,
-            encoding: "UTF-8".to_string(),
-            escape_uri_attribtue: false,
-            html_version: Decimal::ZERO,
-            include_content_type: false,
-            indent: false,
-            item_separator: " ".to_string(),
+            doctype_public,
+            doctype_system,
+            encoding,
+            escape_uri_attributes,
+            html_version,
+            include_content_type,
+            indent,
+            item_separator,
             json_node_output_method: QNameOrString::String("xml".to_string()),
-            media_type: None,
+            media_type,
             method: QNameOrString::String("xml".to_string()),
-            normalization_form: None,
-            omit_xml_declaration: false,
-            standalone: None,
+            normalization_form,
+            omit_xml_declaration,
+            standalone,
             suppress_indentation: Vec::new(),
-            undeclare_prefixes: false,
+            undeclare_prefixes,
             use_character_maps: HashMap::default(),
-            version: "1.0".to_string(),
+            version,
         })
     }
 }
