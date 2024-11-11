acfed8cca409ae4421091c23c3dcf3bebb9ad09d
Martijn Faassen <faassen@startifact.com>
2024-11-11 14:33:11 +0100

    extract xpath into its own module.

--- xee/src/main.rs
+++ xee/src/main.rs
@@ -1,14 +1,9 @@
 mod format;
 mod indent;
+mod xpath;
 
 use clap::{Parser, Subcommand};
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::path::PathBuf;
-use xee_xpath_compiler::{atomic::Atomic, error::SpannedError, evaluate_root, sequence::Item};
-use xot::output::xml::Parameters;
-use xot::Xot;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -49,89 +44,8 @@ fn main() -> anyhow::Result<()> {
             xpath,
             namespace_default,
         } => {
-            let mut xot = Xot::new();
-            let xml_file = File::open(xml).unwrap();
-            let mut buf_reader = BufReader::new(xml_file);
-            let mut xml = String::new();
-            buf_reader.read_to_string(&mut xml).unwrap();
-            let root = xot.parse(&xml).unwrap();
-            let result = evaluate_root(
-                &mut xot,
-                root,
-                &xpath,
-                &namespace_default.unwrap_or(String::new()),
-            );
-            match result {
-                Ok(sequence) => {
-                    for item in sequence.items()? {
-                        display_item(&xot, &item).unwrap();
-                    }
-                }
-                Err(e) => render_error(&xpath, e),
-            }
+            xpath::xpath(xml, xpath, namespace_default)?;
         }
     }
     Ok(())
-}
-
-fn display_item(xot: &Xot, item: &Item) -> Result<(), xot::Error> {
-    match item {
-        Item::Node(node) => {
-            println!("node: \n{}", display_node(xot, *node)?);
-        }
-        Item::Atomic(value) => println!("atomic: {}", display_atomic(value)),
-        Item::Function(function) => println!("function: {:?}", function),
-    }
-    Ok(())
-}
-
-fn display_atomic(atomic: &Atomic) -> String {
-    format!("{}", atomic)
-}
-
-fn display_node(xot: &Xot, node: xot::Node) -> Result<String, xot::Error> {
-    match xot.value(node) {
-        xot::Value::Attribute(attribute) => {
-            let value = attribute.value();
-            let (name, namespace) = xot.name_ns_str(attribute.name());
-            let name = if !namespace.is_empty() {
-                format!("Q{{{}}}{}", namespace, name)
-            } else {
-                name.to_string()
-            };
-            Ok(format!("Attribute {}=\"{}\"", name, value))
-        }
-        xot::Value::Namespace(..) => {
-            todo!()
-        }
-        _ => xot.serialize_xml_string(
-            {
-                Parameters {
-                    indentation: Default::default(),
-                    ..Default::default()
-                }
-            },
-            node,
-        ),
-    }
-}
-
-fn render_error(src: &str, e: SpannedError) {
-    let red = ariadne::Color::Red;
-
-    let mut report =
-        ariadne::Report::build(ariadne::ReportKind::Error, "source", 0).with_code(e.error.code());
-
-    if let Some(span) = e.span {
-        report = report.with_label(
-            ariadne::Label::new(("source", span.range()))
-                .with_message(e.error.message())
-                .with_color(red),
-        )
-    }
-    report
-        .finish()
-        .print(("source", ariadne::Source::from(src)))
-        .unwrap();
-    println!("{}", e.error.note());
 }
