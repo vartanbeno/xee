23266501d324583f6859ad0d6e437904b1822c5a
Martijn Faassen <faassen@startifact.com>
2024-12-04 21:08:42 +0100

    The interpreter now relies on regexml only, not regex, at least in its direct dependencies.

--- xee-interpreter/src/atomic/cast_string.rs
+++ xee-interpreter/src/atomic/cast_string.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regexml::Regex;
 use std::sync::LazyLock;
 
 use xee_xpath_ast::parse_name;
@@ -13,27 +13,33 @@ use super::StringType;
 // https://www.w3.org/TR/xml11/#NT-Nmtoken
 // 	NameStartChar	   ::=   	":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x2FF] | [#x370-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
 // We create the NCName versions without colon (:) so we can do ncnames easily later
-static NCNAME_START_CHAR: &str = r"A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c\u200d\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd\U00010000-\U000effff";
+static NCNAME_START_CHAR: &str = "A-Z_a-z\u{c0}-\u{d6}\u{d8}-\u{f6}\u{f8}-\u{02ff}\u{0370}-\u{037d}\u{037f}-\u{1fff}\u{200c}\u{200d}\u{2070}-\u{218f}\u{2c00}-\u{2fef}\u{3001}-\u{d7ff}\u{f900}-\u{fdcf}\u{fdf0}-\u{fffd}\u{10000}-\u{effff}";
+// static NCNAME_START_CHAR: &str = r"A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c\u200d\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd\U00010000-\U000effff";
 // NameChar	   ::=   	NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
-static NCNAME_CHAR_ADDITIONS: &str = r"-\.0-9\xb7\u0300-\u036F\u203F-\u2040";
-static LANGUAGE_REGEX: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r"^[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*$").expect("Invalid regex"));
+static NCNAME_CHAR_ADDITIONS: &str = "-\\.0-9\u{b7}\u{0300}-\u{036F}\u{203F}-\u{2040}";
+// static NCNAME_CHAR_ADDITIONS: &str = r"-\.0-9\xb7\u0300-\u036F\u203F-\u2040";
+static LANGUAGE_REGEX: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::xpath(r"^[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*$", "").expect("Invalid regex")
+});
 // Nmtoken	 ::= (NameChar)+
 static NMTOKEN_REGEX: LazyLock<Regex> = LazyLock::new(|| {
     // we have to add the colon for NAME_START_CHAR / NAME_CHAR
-    Regex::new(&format!(
-        "^[:{}{}]+$",
-        NCNAME_START_CHAR, NCNAME_CHAR_ADDITIONS
-    ))
+    Regex::xpath(
+        &format!("^[:{}{}]+$", NCNAME_START_CHAR, NCNAME_CHAR_ADDITIONS),
+        "",
+    )
     .expect("Invalid regex")
 });
 // Name	   ::=   	NameStartChar (NameChar)*
 static NAME_REGEX: LazyLock<Regex> = LazyLock::new(|| {
     // we have to add the colon for NAME_START_CHAR / NAME_CHAR
-    Regex::new(&format!(
-        "^[:{}][:{}{}]*$",
-        NCNAME_START_CHAR, NCNAME_START_CHAR, NCNAME_CHAR_ADDITIONS
-    ))
+    Regex::xpath(
+        &format!(
+            "^[:{}][:{}{}]*$",
+            NCNAME_START_CHAR, NCNAME_START_CHAR, NCNAME_CHAR_ADDITIONS
+        ),
+        "",
+    )
     .expect("Invalid regex")
 });
 // https://www.w3.org/TR/xml-names11/#NT-NCName
@@ -41,10 +47,13 @@ static NAME_REGEX: LazyLock<Regex> = LazyLock::new(|| {
 // 	NCNameChar	   ::=   	NameChar - ':'
 //	NCNameStartChar	   ::=   	NameStartChar - ':'
 static NC_NAME_REGEX: LazyLock<Regex> = LazyLock::new(|| {
-    Regex::new(&format!(
-        "^[{}][{}{}]*$",
-        NCNAME_START_CHAR, NCNAME_START_CHAR, NCNAME_CHAR_ADDITIONS
-    ))
+    Regex::xpath(
+        &format!(
+            "^[{}][{}{}]*$",
+            NCNAME_START_CHAR, NCNAME_START_CHAR, NCNAME_CHAR_ADDITIONS
+        ),
+        "",
+    )
     .expect("Invalid regex")
 });
 
