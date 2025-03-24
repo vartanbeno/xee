1471454f5f81f2333225e70b04349585170b6da8
Martijn Faassen <faassen@startifact.com>
2025-03-24 13:56:05 +0100

    Create some basic API documentation.

--- xee-name/src/lib.rs
+++ xee-name/src/lib.rs
@@ -1,7 +1,10 @@
+#![warn(missing_docs)]
+
+//! Manage namespaces in Xee.
+
 mod namespaces;
 mod variable_names;
 
-// pub use name::{Error};
 pub use namespaces::{NamespaceLookup, Namespaces, FN_NAMESPACE, XS_NAMESPACE};
 pub use variable_names::VariableNames;
 pub use xot::xmlname::OwnedName as Name;
